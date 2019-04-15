fb084a490e2fe9c536d2783330db93ca4c3bb95e
Raycel Graterol <craycel_0@hotmail.com>
2019-04-15 14:42:40 -0400

    cookie session local
    cookie session local storage

---
+++ WebApplication1/WebApplication1/wwwroot/js/app/testWebStaff.js
@@ -0,0 +1,27 @@
+﻿var testWebStaff = (function () {
+
+    function setCookie(valueSet) {
+        
+        // Set a cookie
+        $.cookie("name", valueSet);
+        
+        return true;
+    }
+
+    function getCookie(nameCookie) {
+        // Read the cookie
+        var cookieValue = $.cookie(nameCookie);
+
+        return cookieValue;
+    }
+
+    function testCode() {
+        console.log("Tested");
+    }
+
+    return {
+        setCookie: setCookie,
+        getCookie: getCookie,
+        testCode: testCode
+    };
+})();
