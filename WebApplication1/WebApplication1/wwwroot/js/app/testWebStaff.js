06504afe23fa160ef614b2e99bf5916773569996
prometeojose1990 <49495515+prometeojose1990@users.noreply.github.com>
2019-04-15 22:06:41 +0200

    jg test browser stoage
    save storage

--- WebApplication1/WebApplication1/wwwroot/js/app/testWebStaff.js
+++ WebApplication1/WebApplication1/wwwroot/js/app/testWebStaff.js
@@ -1,27 +1,59 @@
 ﻿var testWebStaff = (function () {
-
     function setCookie(valueSet) {
         
         // Set a cookie
-        $.cookie("name", valueSet);
-        
-        return true;
+        _setCookie("name", valueSet ,10)
     }
 
+   
     function getCookie(nameCookie) {
         // Read the cookie
-        var cookieValue = $.cookie(nameCookie);
-
+        var cookieValue = _getCookie(nameCookie);//
         return cookieValue;
-    }
+    }      
+
+    //jQuery
+    /*
+    $.cookie("test", 1, {
+        expires: 10,          
+
+        path: '/',         
+
+        domain: 'jquery.com', 
+
+        secure: true         
+    });*/
 
     function testCode() {
         console.log("Tested");
     }
 
+    function setLocalStorage(name,valueS) {
+        localStorage.setItem(name, valueS);
+    }
+
+    function getLocalStorage(name) {
+        localStorage.getItem(name);
+    }
+
+    function setSessionStorage(name, valueS) {
+        sessionStorage.setItem(name, valueS);
+    }
+
+    function getSessionStorage(name) {
+        return sessionStorage.getItem(name);
+    }
+
+
+
     return {
         setCookie: setCookie,
         getCookie: getCookie,
-        testCode: testCode
+        testCode: testCode,
+        setLS: setLocalStorage,
+        getLS: getLocalStorage,
+        setSS: setSessionStorage,
+        getSS: getSessionStorage
+        
     };
 })();
--- WebApplication1/WebApplication1/wwwroot/js/site.js
+++ WebApplication1/WebApplication1/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function _getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function _setCookie(cname, cvalue, exdays) {
+    if (cvalue !== null || cvalue !== undefined) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+}
